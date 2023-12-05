dd97c9cd4ad3d91dc4d8bc7d951dfc6a3c7e5fe0
ParkSiHyeong <parksh4603@naver.com>
2023-12-05 16:19:30 +0900

    init: 프로젝트 업로드

---
+++ spring-message-i18n-sutdy/src/main/java/com/example/springmessagei18nsutdy/SpringMessageI18nSutdyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmessagei18nsutdy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMessageI18nSutdyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMessageI18nSutdyApplication.class, args);
+	}
+
+}
---
+++ spring-message-i18n-sutdy/src/test/java/com/example/springmessagei18nsutdy/SpringMessageI18nSutdyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmessagei18nsutdy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMessageI18nSutdyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
